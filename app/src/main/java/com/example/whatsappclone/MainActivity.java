77ba7729969483f98c500048a3e9ea97909beaa9
Ankit <singhankit1302@gmail.com>
2022-12-25 13:21:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+        tabLayout = findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+}
